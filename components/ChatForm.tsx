81d55a0a655fb3a691ddaf70b8e06b2598e005d8
Estra-Dev <dominikolyson@gmail.com>
2025-04-22 19:46:32 +0100

    update input

--- components/ChatForm.tsx
+++ components/ChatForm.tsx
@@ -11,7 +11,7 @@ const ChatForm = ({
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    if (message.trim() !== "") {
+    if (message.trim()) {
       onSendMessage(message);
       setMessage("");
     }
@@ -21,6 +21,7 @@ const ChatForm = ({
     <form onSubmit={handleSubmit} className=" flex gap-2 mt-4">
       <input
         type="text"
+        value={message || ""}
         placeholder="Enter your message..."
         className=" flex-1 px-4 border-[1px] py-2 rounded-lg focus:outline-none"
         onChange={(e) => setMessage(e.target.value)}
