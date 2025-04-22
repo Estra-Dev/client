07e36d9b723036939ed77a4f9f95959798064e8d
Estra-Dev <dominikolyson@gmail.com>
2025-04-22 17:55:26 +0100

    chat-room frntend

---
+++ components/ChatForm.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import React, { useState } from "react";
+
+const ChatForm = ({
+  onSendMessage,
+}: {
+  onSendMessage: (message: string) => void;
+}) => {
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (message.trim() !== "") {
+      onSendMessage(message);
+      setMessage("");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className=" flex gap-2 mt-4">
+      <input
+        type="text"
+        placeholder="Enter your message..."
+        className=" flex-1 px-4 border-[1px] py-2 rounded-lg focus:outline-none"
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button
+        type="submit"
+        className=" px-4 py-2 bg-blue-500 rounded-lg text-white"
+      >
+        {" "}
+        Send
+      </button>
+    </form>
+  );
+};
+
+export default ChatForm;
