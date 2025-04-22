78ac0db96a950624a09b1703b70715ddfeeef6ff
Estra-Dev <dominikolyson@gmail.com>
2025-04-22 18:08:58 +0100

    update env

--- app/lib/socketClient.ts
+++ app/lib/socketClient.ts
@@ -1,4 +1,7 @@
 "use client";
 import { io } from "socket.io-client";
 
-export const socket = io(process.env.NEXT_PUBLIC_SOCKET_SERVER_URL);
+export const socket = io(
+  process.env.NEXT_PUBLIC_SOCKET_SERVER_URL ||
+    process.env.NEXT_PUBLIC_SOCKET_SERVER_URL_2
+);
